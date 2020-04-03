44d3a5cbc43b45dff396feb1b5387c36dd8de569
Mika <mika.salinkangas@semak.fi>
2020-04-03 20:49:39 +0300

    commit

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const port = process.env.PORT || 8080;
+const mongoose = require('mongoose');
 const app = express();
 const body_parser = require('body-parser')
 
@@ -27,6 +28,21 @@ app.get("/api/materials", material_controller.api_get_materials);
 
 
 // DELETE
+app.delete("/api/material/:id", material_controller.api_delete_material);
 
 
-app.listen(port);
+
+//Bwhx7kQeLlmGIGYq
+const database_uri = "mongodb+srv://server:Bwhx7kQeLlmGIGYq@cluster0-wicfr.mongodb.net/materialdb?retryWrites=true&w=majority";
+
+mongoose.connect(database_uri, {
+    useCreateIndex: true,
+    useUnifiedTopology: true,
+    useNewUrlParser: true,
+    useFindAndModify: false
+}).then(()=>{
+    console.log('database connected')
+    app.listen(port);
+}).catch(err=>{
+    console.log(err);
+});
--- material_controller.js
+++ material_controller.js
@@ -1,16 +1,42 @@
-
+const material_model = require('./material_model');
 
 // CREATE
 const api_post_material = (req,res,next) =>{
-    let data = req.body;
-    console.log(data);
-    res.send(JSON.stringify(data));
+    let data = {
+        name: req.body.name,
+        min_density: req.body.min_density,
+        max_density: req.body.max_density,
+        min_strength: req.body.min_strength,
+        max_strength: req.body.max_strength,
+        min_strength_density: req.body.min_strength / req.body.max_density,
+        max_strength_density: req.body.max_strength / req.body.min_density
+    }
+
+    let new_material = material_model(data);
+    new_material.save().then(()=>{
+        console.log(new_material);
+        res.send(JSON.stringify(new_material));
+    }).catch(err =>{
+        res.status(500);
+        res.send(err.errmsg);
+        console.log(err);
+    });
+
 };
 
 
 // READ
 const api_get_materials = (req,res,next) =>{
-    res.send(JSON.stringify([]));
+
+    material_model.find({})
+    .lean()
+    .then((materials) =>{
+        res.send(JSON.stringify(materials));
+    }).catch(err =>{
+        res.status(500);
+        res.send(err.errmsg);
+        console.log(err);
+    });
 };
 
 
@@ -19,9 +45,26 @@ const api_get_materials = (req,res,next) =>{
 
 
 // DELETE
-
+const api_delete_material = (req,res,next)=>{
+    let id = req.params.id;
+    material_model.findOneAndRemove(id).then(()=>{
+        res.send();
+    }).catch(err =>{
+        res.status(500);
+        res.send(err.errmsg);
+        console.log(err);
+    });
+/*     material_model.findByIdAndRemove(id).then(()=>{
+        res.send();
+    }).catch(err =>{
+        res.status(500);
+        res.send(err.errmsg);
+        console.log(err);
+    }); */
+};
 
 
 // EXPORTS
 module.exports.api_post_material = api_post_material;
 module.exports.api_get_materials = api_get_materials;
+module.exports.api_delete_material = api_delete_material;
