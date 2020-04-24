25d062ccbf4447e5a6f2ea1be737c5a415112834
Mika <mika.salinkangas@semak.fi>
2020-04-24 17:01:21 +0300

    commit

---
+++ public/materials_table.js
@@ -0,0 +1,72 @@
+$(document).ready(()=>{
+    let table  = $("#materials_table").DataTable({
+        ajax: {
+            type: "GET",
+            datatype: "json",
+            url: "/api/materials",
+            dataSrc: ""
+        },
+        rowId: "_id",
+        columns: [
+            {data: "_id", type: "readonly", visible: false},
+            {data: "name", type: "text", required: true},
+            {data: "min_density", type: "float", required: true},
+            {data: "max_density", type: "float", required: true},
+            {data: "min_strength", type: "float", required: true},
+            {data: "max_strength", type: "float", required: true},
+            {data: "min_strength_density", type: "readonly"},
+            {data: "max_strength_density", type: "readonly"},
+        ],
+        dom: "Bfrtip",
+        select: "single",
+        responsive: true,
+        altEditor: true,
+        buttons: [
+            //"columnsToggle",
+            {
+                text: "Create",
+                name: "add"
+            },
+            {
+                text: "Edit",
+                name: "edit"
+            },
+            {
+                text: "Delete",
+                name: "delete"
+            },
+            {
+                text: "Refresh",
+                name: "refresh"
+            }
+        ],
+        onAddRow: (datatable, rowdata, success, error)=>{
+            $.ajax({
+                url: "/api/material",
+                type: "POST",
+                data: rowdata,
+                success: success,
+                error: error
+            });
+        },
+        onDeleteRow: (datatable, rowdata, success, error)=>{
+            $.ajax({
+                url: "/api/material/" + rowdata._id,
+                type: "DELETE",
+                data: rowdata,
+                success: success,
+                error: error
+            });
+        },
+        onEditRow: (datatable, rowdata, success, error)=>{
+            $.ajax({
+                url: "/api/material/" + rowdata._id,
+                type: "PUT",
+                data: rowdata,
+                success: success,
+                error: error
+            });
+        }
+    });
+});
+
